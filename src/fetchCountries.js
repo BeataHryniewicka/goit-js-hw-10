2005454571a8f46885bc6827d87a472d3fa499e5
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-04-29 23:23:27 +0200

    .

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,29 +1,47 @@
-import Notiflix from 'notiflix';
 export { fetchCountries };
-
+import Notiflix from 'notiflix';
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 //definicja funkcji, która wysyła żądanie HTTP do endpointa (name.official,capital,population,flags,languages) z argumentem name jako nazwą kraju, którą użytkownik wpisuje w formularzu
-// async function fetchCountries() {
-//   //pobiera dane z serwera w formacie JSON za pomocą metody fetch()
-//   const response = await fetch(
-//     'https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages'
-//   );
-//   // przekazuje obietnicę której wynikiem będzie tablica krajów będącą wynikiem żądania
-//   //metoda response.json() przetwarza odpowiedź i zwraca obiekt js jako tablicę krajów(data).
-//   const data = await response.json();
-//   // if (data.length > 10) {
-//   // console.log('error10');
-//   //Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
-//   //}
-//   return data;
-// }
-
 async function fetchCountries() {
+  //pobiera dane z serwera w formacie JSON za pomocą metody fetch()
   return await fetch(
     'https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages'
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  )
+    .then(response => {
+      if (!response.ok) {
+        if (response === 404) {
+          throw new Error(404);
+        } else {
+          throw new Error('No results founded');
+        }
+      }
+      // przekazuje obietnicę której wynikiem będzie tablica krajów będącą wynikiem żądania
+      // metoda response.json() przetwarza odpowiedź i zwraca obiekt js jako tablicę krajów.
+      return response.json();
+    })
+    .then(data => {
+      countryListEl.innerHTML = '';
+      countryInfoEl.innerHTML = '';
+      if (data.length > 10) {
+        Notiflix.Notify.failure(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length >= 2 && data.length <= 10) {
+        filteredList(countries);
+      } else if ((data.length = 1)) {
+        filteredCountry();
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 }
+
+// .then(countries => {
+//   filteredList(countries);
+// })
+// .catch(error => {
+//   //console.log('error input');
+//   Notiflix.Notify.failure('Oops, there is no country with that name');
+// });
--- src/index.js
+++ src/index.js
@@ -7,33 +7,44 @@ const DEBOUNCE_DELAY = 300;
 const inputEl = document.getElementById('search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
-let flagImg;
 
-//countryInfoEl.innerHtml = `<img class="flag-list" src="${country.flags.svg}"/> ;
+const flagEl = document.createElement('img');
+//flagEl.src =  ${country.flags.svg};
+//flagEL.alt = `Flag of ${country.name.official}`;
+
+function filteredCountry(country) {
+  const countryInfo = `<li>
+<img class="flag" src="${country.flags.svg}" width="100" alt = "Flag of ${country.name.official}"/>;
+<p><b>name: ${country.name.oficcial}</b></p>
+<p><b>capital: ${country.capital}</b></p>
+<p><b>population: ${country.population}</b></p>
+<p><b>languages: ${country.languages}</b></p>
+</li>`;
+  countryInfoEl.innerHTML = countryInfo;
+}
+// filteredCountry();
+
 function filteredList(countries) {
   const markup = countries
     .map(country => {
       return `<li class="cournriesList">
-      <img class="flag" src="${country.flags.svg}" width="100"/>
-       <p> <b> </b>${country.name.official}</p></b> </li>
+      <img class="flag" src="${country.flags.svg}" width="100" alt = "Flag of ${country.name.official}"/>
+       <p> <b>${country.name.official}</p></b> </li>
               `;
     })
     .join('');
 
   countryListEl.innerHTML = markup;
 }
-//<p>capital: ${country.capital}</p>
-//<p>population: ${country.population}</p>
-// <p>languages: ${country.languages}</p>
 
 inputEl.addEventListener('input', () => {
   // console.log('error input');
-  fetchCountries()
-    .then(countries => {
-      filteredList(countries);
-    })
-    .catch(error => {
-      console.log('error input');
-      //Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+  fetchCountries();
+  // .then(countries => {
+  //   filteredList(countries);
+  // })
+  // .catch(error => {
+  //   //console.log('error input');
+  //   Notiflix.Notify.failure('Oops, there is no country with that name');
+  // });
 });
